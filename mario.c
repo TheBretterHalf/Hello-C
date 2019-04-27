2430dddc9a9d1cf71208b8268f3299db91963199
Brett Stainken <brettstainken.student@careerdevs.com>
2019-04-27 22:37:24 +0000

    Beginning of challenging mario

---
+++ mario.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height = get_int("Height :\n");
+    do {
+      printf("value of a: %d\n", int);
+   }while( height = 1 );
+   return "# #";
+    do {
+      printf("value of a: %d\n", int);
+   }while( height = 2 );
+   return "# #\n## ##";
+    do {
+      printf("value of a: %d\n", int);
+   }while( a = 3 );
+   return "# #\n## ##\n### ###";
+    do {
+      printf("value of:%d\n", int);
+   }while( a = 4 );
+   return "# #\n## ##\n### ###\n#### ####";
+    do {
+      printf("value of: %d\n", int);
+   }while( a = 5 );
+   return "# #\n## ##\n### ###\n#### ####\n##### #####";
+}
