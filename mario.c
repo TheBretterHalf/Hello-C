04f933b12b4ba93f43e1d431a94773d98682e4d0
Brett Stainken <brettstainken.student@careerdevs.com>
2019-04-29 23:15:15 +0000

    mario

--- mario.c
+++ mario.c
@@ -5,6 +5,19 @@ int main (void)
 {
     int i, space, k=0;
     int height = get_int("Height :\n");
-
+    for (i=1; i<=height; i++; k=0);
+    {
+        for (space=1; space<=rows-i; space++)
+        {
+            printf(" ");
+        }
+        while (k!=i-1)
+        {
+            printf("* ");
+            ++k;
+        }
+        printf("\n");
+    }
+    return 0;
 }
 
