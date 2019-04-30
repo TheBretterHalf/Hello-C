b09c6f7f130410844036652bf08cbadbabaad0e0
Brett Stainken <brettstainken.student@careerdevs.com>
2019-04-30 18:47:54 +0000

    martoggle

--- mario.c
+++ mario.c
@@ -5,31 +5,33 @@ int main(void)
 {
     int i, j, space, height, k = 0;
     do
+    {
+        height = get_int("Height : ");
+        if (height>0 && height<9)
         {
-            height = get_int("Height : ");
-            if (height>0 && height<9)
+            for (i = 2; i <= height + 1; i++, k = 0)
             {
-                height=height+1;
-                for (i = 1; i <= height; i++, k = 0)
+                for (space = 0; space <= height - i; space++)
                 {
-                    for (space = 0; space <= height - i; space++)
-                    {
-                        printf(" ");
-                    }
-                    while (k != i - 1)
-                    {
-                        printf("#");
-                        ++k;
-                    }
                     printf(" ");
-                    for (j = 1; j <= k; j++)
-                    {
-                        printf("#");
-                    }
-                    printf("\n");
                 }
-            } else{}
-        }while (height>0 && height<9);
+                while (k != i - 1)
+                {
+                    printf("#");
+                    ++k;
+                }
+                printf(" ");
+                for (j = 1; j <= k; j++)
+                {
+                    printf("#");
+                }
+                printf("\n");
+            }
+        }else{
+            height = get_int("Height : ");
+        }
+    }
+    while (height > 0 && height < 9);
     return 0;
 }
 
