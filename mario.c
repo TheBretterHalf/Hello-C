d28ec485397ac4a93f0c8f9cf7c53129af5b3749
Brett Stainken <brettstainken.student@careerdevs.com>
2019-04-30 20:47:22 +0000

    marhopefinal

--- mario.c
+++ mario.c
@@ -3,37 +3,35 @@
 
 int main(void)
 {
-    int i, j, space, height, k = 0;
+    int i, j, space, k = 0;
+    int height = 0;
     do
     {
         height = get_int("Height : ");
-        if (height > 0 && height < 9)
+    }
+    while (height > 8 || height < 1);
+    do
+    {
+
+        for (i = 2; i <= height + 1; i++, k = 0)
         {
-            for (i = 2; i <= height + 1; i++, k = 0)
+            for (space = 0; space <= height - i; space++)
             {
-                for (space = 0; space <= height - i; space++)
-                {
-                    printf(" ");
-                }
-                while (k != i - 1)
-                {
-                    printf("#");
-                    ++k;
-                }
                 printf(" ");
-                for (j = 1; j <= k; j++)
-                {
-                    printf("#");
-                }
-                printf("\n");
             }
-        }
-        else
-        {
-            height = get_int("Height : ");
+            while (k != i - 1)
+            {
+                printf("#");
+                ++k;
+            }
+            printf("  ");
+            for (j = 1; j <= k; j++)
+            {
+                printf("#");
+            }
+            printf("\n");
         }
     }
-    while (height > 0 && height < 9);
+    while (height < 1 && height > 9);
     return 0;
 }
-
