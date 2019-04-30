755234e1d4a2e03f1e73857612dc3df3047f451f
Brett Stainken <brettstainken.student@careerdevs.com>
2019-04-30 14:49:16 +0000

    MarComplete

--- mario.c
+++ mario.c
@@ -1,23 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
-    int i, j, space, k=0;
+    int i, j, space, k = 0;
     int height = get_int("Height :\n");
-    for (i=1; i<=height; i++, k=0)
+    for (i = 1; i <= height; i++, k = 0)
     {
-        for (space=1; space<=height-i; space++)
+        for (space = 1; space <= height - i; space++)
         {
             printf(" ");
         }
-        while (k!=i-1)
+        while (k != i - 1)
         {
             printf("#");
             ++k;
         }
         printf(" ");
-        for (j=1; j<=height; j++)
+        for (j = 1; j <= k; j++)
         {
             printf("#");
         }
