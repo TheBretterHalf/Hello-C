16c0b51077a2ce557d04cc716989c08c0947abfc
Brett Stainken <brettstainken.student@careerdevs.com>
2019-05-01 20:57:05 +0000

    cash

--- cash.c
+++ cash.c
@@ -7,15 +7,34 @@ int main (void)
     int coins=0;
     float cash;
     int money;
+    int i;
+    int quarters;
+    int nickels;
+    int dimes;
+    int pennies;
+    int changeq;
+    int changed;
     do
     {
         cash = get_float("Cash: ");
         money=cash*100;
-        printf("%i\n", money);
     } while (money<0);
-    if (money>=25){
-        money=money-25;
-        coins++;
-        printf("%i\n", coins);
-    }else{}
+    if(money>=25)
+    {
+        quarters=money/25;
+        printf("quarters: %i\n", quarters);
+        changeq=money%25;
+        if (changeq>=10)
+        {
+            dimes=changeq/10;
+            printf("dimes: %i\n", dimes);
+            changed=changeq%10;
+            if (changed>=5)
+            {
+                nickels=changed/5;
+                printf("nickels: %i\n", nickels);
+                changen=changed%5;
+            }
+        }
+    }
 }
