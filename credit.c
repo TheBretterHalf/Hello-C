b81baa906559af38e8462359bce8168710e3f635
Brett Stainken <brettstainken.student@careerdevs.com>
2019-05-02 16:51:44 +0000

    credit

--- credit.c
+++ credit.c
@@ -2,7 +2,14 @@
 #include <math.h>
 #include <stdio.h>
 
+long-long number;
+
 int main(void)
 {
+    do
+    {
+        number = get_long_long("Number: ");
+    }
+    while(number<1000000000000 || number>10000000000000000);
 
 }
