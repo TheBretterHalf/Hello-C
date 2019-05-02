0bfa3bc3f735dad9b2c10991c96a3ed6be247260
Brett Stainken <brettstainken.student@careerdevs.com>
2019-05-02 23:47:33 +0000

    credit

--- credit.c
+++ credit.c
@@ -14,8 +14,30 @@ int main(void)
         {
             numdigits = floor(log10(number)) + 1;
         }
-        printf("%i\n", numdigits);
+        //printf("%i\n", numdigits);
     }
-    while(numdigits!=16 || numdigits!=13 || numdigits!=15);
+    while(numdigits!=15 && numdigits!=13 && numdigits!=16 );
+    // printf("outofloop\n");
+    // return 0;
+    //for (int i=0; i<=numdigits; i++)
+    //{
+        //printf("%i", i);
 
+    //}
+    long long int odddigits=number;
+    long long int evendigits=number;
+    int oddtotal=0;
+    int eventotal=0;
+    while(odddigits != 0)
+    {
+        int digit = odddigits % 10;
+        odddigits = odddigits / 100;
+        //printf("%d\n", digit);
+        oddtotal=oddtotal+digit;
+        //printf("%d\n", oddtotal);
+        int edigit = evendigits % 100;
+        evendigits = evendigits /100;
+        printf("%d", edigit);
+    }
+    printf("%d\n", oddtotal);
 }
