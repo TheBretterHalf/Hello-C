1db8d60238659cc4573b37048a1b7e4acc8d42e3
Brett Stainken <brettstainken.student@careerdevs.com>
2019-05-03 19:34:09 +0000

    credit finish

--- credit.c
+++ credit.c
@@ -16,91 +16,97 @@ int main(void)
         }
         //printf("%i\n", numdigits);
     }
-    while(numdigits!=15 && numdigits!=13 && numdigits!=16 );
-    // printf("outofloop\n");
-    // return 0;
-    //for (int i=0; i<=numdigits; i++)
-    //{
-        //printf("%i", i);
+    while(numdigits<0);
+    if (numdigits==15 || numdigits ==16 || numdigits ==13)
+    {
+        // printf("outofloop\n");
+        // return 0;
+        //for (int i=0; i<=numdigits; i++)
+        //{
+            //printf("%i", i);
 
-    //}
-    long long int odddigits=number;
-    long long int evendigits=number;
-    int oddtotal=0;
-    int eventotal=0;
-    while(odddigits > 0)
-    {
-        int digit = odddigits % 10;
-        odddigits = odddigits / 100;
-        //printf("%d\n", digit);
-        oddtotal=oddtotal+digit;
-        //printf("%d\n", oddtotal);
-        int edigit = (evendigits/10) % 10;
-        evendigits = evendigits /100;
-        //printf("%d\n", edigit);
-        edigit=2*edigit;
-        if(edigit>9)
+        //}
+        long long int odddigits=number;
+        long long int evendigits=number;
+        int oddtotal=0;
+        int eventotal=0;
+        while(odddigits > 0)
         {
-            edigit=edigit/10+edigit%10;
+            int digit = odddigits % 10;
+            odddigits = odddigits / 100;
+            //printf("%d\n", digit);
+            oddtotal=oddtotal+digit;
+            //printf("%d\n", oddtotal);
+            int edigit = (evendigits/10) % 10;
+            evendigits = evendigits /100;
             //printf("%d\n", edigit);
+            edigit=2*edigit;
+            if(edigit>9)
+            {
+                edigit=edigit/10+edigit%10;
+                //printf("%d\n", edigit);
+            }
+            //printf("%d\n", edigit);
+            eventotal= eventotal+edigit;
+            //printf("%d\n", eventotal);
         }
-        //printf("%d\n", edigit);
-        eventotal= eventotal+edigit;
+        //printf("%d\n", oddtotal);
         //printf("%d\n", eventotal);
-    }
-    //printf("%d\n", oddtotal);
-    //printf("%d\n", eventotal);
-    int sumcheck = (eventotal+oddtotal)%10;
-    //printf("%d\n", sumcheck);
-    if (sumcheck==0)
-    {
-        if (numdigits==15)
+        int sumcheck = (eventotal+oddtotal)%10;
+        //printf("%d\n", sumcheck);
+        if (sumcheck==0)
         {
-            long long int amexfirsttwo=number/pow(10, numdigits-2);
-            //printf("%lli\n", amexfirsttwo);
-            if(amexfirsttwo==34 || amexfirsttwo==37)
+            if (numdigits==15)
             {
-                printf("AMEX\n");
+                long long int amexfirsttwo=number/pow(10, numdigits-2);
+                //printf("%lli\n", amexfirsttwo);
+                if(amexfirsttwo==34 || amexfirsttwo==37)
+                {
+                    printf("AMEX\n");
+                }
+                else
+                {
+                    printf("INVALID\n");
+                }
             }
-            else
+            if (numdigits==16)
             {
-                printf("INVALID\n");
+                long long int masterfirsttwo=number/pow(10, numdigits-2);
+                //printf("%lli\n", masterfirsttwo);
+                long long int visafirstone=number/pow(10,numdigits-1);
+                if (masterfirsttwo>50 && masterfirsttwo<56)
+                {
+                    printf("MASTERCARD\n");
+                }
+                if (visafirstone==4)
+                {
+                    printf("VISA\n");
+                }
+                else
+                {
+                    printf("INVALID\n");
+                }
+            }
+            if(numdigits==13)
+            {
+                long long int visafirstone=number/pow(10,numdigits-1);
+                if (visafirstone==4)
+                {
+                    printf("VISA\n");
+                }
+                else
+                {
+                    printf("INVALID\n");
+                }
             }
         }
-        if (numdigits==16)
+        else
         {
-            long long int masterfirsttwo=number/pow(10, numdigits-2);
-            //printf("%lli\n", masterfirsttwo);
-            long long int visafirstone=number/pow(10,numdigits-1);
-            if (masterfirsttwo>50 && masterfirsttwo<56)
-            {
-                printf("MASTERCARD\n");
-            }
-            if (visafirstone==4)
-            {
-                printf("VISA\n");
-            }
-            else
-            {
-                printf("INVALID\n");
-            }
-        }
-        if(numdigits==13)
-        {
-            long long int visafirstone=number/pow(10,numdigits-1);
-            if (visafirstone==4)
-            {
-                printf("VISA\n");
-            }
-            else
-            {
-                printf("INVALID\n");
-            }
+            printf("INVALID\n");
         }
     }
     else
     {
         printf("INVALID\n");
     }
-
 }
